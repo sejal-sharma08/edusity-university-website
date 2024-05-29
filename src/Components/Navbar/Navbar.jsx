05024b885beda59e691ed3a999b9b2e50573d3cd
sejal-sharma08 <sejalshrm08@gmail.com>
2024-05-29 13:06:31 +0530

    spell changed of testimonials

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -29,7 +29,7 @@ const toggleMenu = () => {
             <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
             <li><Link to='about' smooth={true} offset={-150} duration={500}>About us</Link></li>
             <li><Link to='campus' smooth={true} offset={-260} duration={500}>Campus</Link></li>
-            <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Trestimonials</Link></li>
+            <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
             <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
         </ul>
         <img src={menu_icon} alt=""  className='menu-icon' onClick={toggleMenu}/>
